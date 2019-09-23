802f9366f46d80a4f0bd8af11e4224611abc9134
Shivam Saxena <shvmsxn41@gmail.com>
2019-09-24 00:38:43 +0530

    bla bla

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 import pygame
 import time
 import random
+from nnlib import nn
+import numpy as np
+import copy
+class car:
+    def __init__(self,width,height,carImg,brain=None):
+        self.x=width*0.45
+        self.y=height*0.8
+        self.fitness=0
+        self.carImg=carImg
+        if(brain==None):
+            self.brain=nn([4,4,4,3],['leaky_relu','leaky_relu','leaky_relu','softmax'],4)
+        else:
+            self.brain=brain
+    def predict(self,input):
+        input=np.array(input).reshape(4,1)
+        out=self.brain.predict(input)
+        print(out)
+        return np.argmax(out)
+    def re_init(self,width,height):
+        self.x=width*0.5
+        self.y=height*0.8
 class game:  
     def __init__(self):
         pygame.init()
@@ -18,6 +39,7 @@ class game:
         pygame.display.set_caption('Space Impact')
         self.clock = pygame.time.Clock()
         self.carImg = pygame.image.load('images/car.png')
+        self.Car=car(self.display_width,self.display_height,self.carImg)
 
     def things_dodged(self,count):
         font = pygame.font.SysFont(None, 25)
@@ -27,12 +49,11 @@ class game:
     def things(self,thingx, thingy, thingw, thingh, color):
         pygame.draw.rect(self.gameDisplay, color, [thingx, thingy, thingw, thingh])
 
-    def car(self,x,y):
-        self.gameDisplay.blit(self.carImg,(x,y))
-
     def text_objects(self,text, font):
         textSurface = font.render(text, True, self.black)
         return textSurface, textSurface.get_rect()
+    def displayCar(self):
+        self.gameDisplay.blit(self.Car.carImg,(self.Car.x,self.Car.y))
 
     def message_display(self,text):
         largeText = pygame.font.Font('freesansbold.ttf',115)
@@ -65,23 +86,19 @@ class game:
                 if event.type == pygame.QUIT:
                     self.gameExit=True
                     pygame.quit()
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_LEFT:
-                        x_change = -5
-                    if event.key == pygame.K_RIGHT:
-                        x_change = 5
-                if event.type == pygame.KEYUP:
-                    if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                        x_change = 0
             if self.gameExit==True:
                 break
-            x += x_change
+            output=self.Car.predict([random.randint(1,100),random.randint(1,100),random.randint(1,100),random.randint(1,100)])
+            if(output==1): x_change=-5
+            if(output==2): x_change=5
+            self.Car.x += x_change
             self.gameDisplay.fill(self.white)
             self.things(thing_startx, thing_starty, thing_width, thing_height, self.block_color)
             thing_starty += thing_speed
-            self.car(x,y)
+            self.displayCar()
             self.things_dodged(dodged)
-            if x > self.display_width - self.car_width or x < 0:
+            if self.Car.x > self.display_width - self.car_width or self.Car.x < 0:
+                self.Car.x=self.display_width*0.45
                 self.crash()
 
             if thing_starty > self.display_height:
@@ -91,8 +108,9 @@ class game:
                 thing_speed += 1
                 thing_width += (dodged * 1.2)
 
-            if y < thing_starty+thing_height:
-                if x > thing_startx and x < thing_startx + thing_width or x+self.car_width > thing_startx and x + self.car_width < thing_startx+thing_width:
+            if self.Car.y < thing_starty+thing_height:
+                if self.Car.x > thing_startx and self.Car.x < thing_startx + thing_width or self.Car.x+self.car_width > thing_startx and self.Car.x + self.car_width < thing_startx+thing_width:
+                    self.Car.x=self.display_width*0.45
                     self.crash()
             if self.gameExit==False:
                 pygame.display.update()
---
+++ nnlib.py
@@ -0,0 +1,97 @@
+import numpy as np
+import pandas as pd
+import copy
+class nn:
+    #Scaling function
+    def scale(self,X):
+        mean=np.mean(X,axis=0)
+        var=np.var(X,axis=0)
+        X=X-mean
+        X/=(var)**0.5
+        return X
+    #Activation functions and their derivatives
+    #Sigmoid
+    def sigmoid(self,z):
+        return 1/(1+(np.exp(-z)))
+    def sigmoid_derivative(self,a):
+        return self.sigmoid(a)*(1-self.sigmoid(a))
+    
+    #tanh
+    def tanh(self,z):
+        return (np.exp(z)-np.exp(-z))/(np.exp(z)+np.exp(-z))
+    def tanh_derivative(self,a):
+        return 1-self.tanh(a)**2
+    
+    #relu
+    def relu(self,z):
+        out=copy.deepcopy(z)
+        out[out<0]=0
+        return out
+    def relu_derivative(self,a):
+        out=copy.deepcopy(a)
+        out[out>0]=1
+        out[out<=0]=0
+        return out
+    
+    #leaky_relu
+    def leaky_relu(self,z,c=0.01):
+        out=copy.deepcopy(z)
+        return np.where(out>0, out, out * c)
+    def leaky_relu_derivative(self,a,c=0.01):
+        out=np.ones_like(a)
+        out[a<0]=c
+        return out
+    
+    #Softmax
+    def softmax(self,z):
+        t=np.exp(z)
+        out=t/sum(t)
+        return out
+    #Softmax derivative(Not actually derivative but helper function to find dz)
+    def softmax_derivative(self,y,a):
+        return a-y
+    
+    #Loss function and its derivative
+    def log_loss(self,y,a):
+        return (-y *np.log(a+10**-8)-(1-y)*np.log(1 - a+10**-8)).mean()
+    
+    def log_loss_derivative(self,y,a):
+        return (-y/a)+((1-y)/(1-a))
+    
+    #Random Weights Initialisation function
+    def init_weights(self,nodes,features):
+        return np.random.randn(nodes,features)
+    
+    
+    def __init__(self,nodes, activations,features):
+        np.random.seed(0)
+        self.nodes=nodes
+        self.activations=activations    
+        self.act_func={'sigmoid':self.sigmoid,'tanh':self.tanh,'relu':self.relu,'leaky_relu':self.leaky_relu,'softmax':self.softmax}
+        self.act_func_der={'sigmoid':self.sigmoid_derivative,'tanh':self.tanh_derivative,'relu':self.relu_derivative,
+                  'leaky_relu':self.leaky_relu_derivative,'softmax':self.softmax_derivative}
+        self.w=[0]
+        self.b=[0]
+        for layer in range(len(self.nodes)):
+            self.w.append(self.init_weights(self.nodes[layer],features))
+            self.b.append(np.zeros((self.nodes[layer],1)))
+            features=self.nodes[layer]
+                
+    def predict(self,X):
+        X=self.scale(X)
+        Layers_num=len(self.nodes)
+        z=[0]
+        a=[X]
+        for layer in range(1,Layers_num+1):
+            z.append(np.dot(self.w[layer],a[layer-1])+self.b[layer])
+            a.append(self.act_func[self.activations[layer-1]](z[layer]))
+        
+        output=copy.deepcopy(a[-1])
+        return output.T
+
+'''=============================================================================================================================='''
+'''=============================================================================================================================='''
+
+#brain=nn([4,3],['relu','softmax'],4)
+#input=np.array([1,2,3,4]).reshape(4,1)
+#print(brain.predict(input))
